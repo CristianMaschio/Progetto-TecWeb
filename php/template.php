c920d2f2facc6b620c41ee9672b3aaa72a9c4db9
phreppo <parolini.francesco.1@gmail.com>
2017-11-22 16:06:15 +0100

    aggiunte funzionalità

---
+++ php/template.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+
+<?php require_once('php/config.php'); ?>
+<?php require_once('php/printTemplate.php') ?>
+
+<html lang="it" >
+<head>
+  <?= printHead('Pagina template'); ?>
+</head>
+<body>
+
+  <header>
+    <?= printHeader(); ?>
+  </header>
+
+  <nav>
+    <?= printNavBar(); ?>
+  </nav>
+
+  <div id="content">
+  </div>
+
+  <footer>
+    <?= printFooter(); ?>
+  </footer>
+</body>
+</html>
