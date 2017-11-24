a06d14653a1da71a37c4131556e02110f34dac49
Unknown <parolini.francesco.1@gmail.com>
2017-11-24 17:57:46 +0100

    Aggiunto in congif display di errori php

--- php/php/config.php
+++ php/php/config.php
@@ -1,5 +1,10 @@
 <?php
 
+// TODO: sarà da togliere prima di consegnare, lo metto finchè scriviamo per avere un feedback
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 /*
 VARIABILI IN SESSIONE CON UTENTE LOGGATO
 
