aa239cb8c589e28c50df52ed120cbf2cdbb5b363
CristianMaschio <blackspartan@hotmail.it>
2017-11-27 15:58:26 +0100

    Add css modifica categoria fiera

--- php/categoria_mod.php
+++ php/categoria_mod.php
@@ -27,12 +27,13 @@ register('id_mod');
     <?php
     $cercato = select("SELECT * FROM categorie WHERE id=$id_mod")[0];
     ?>
-    <h2>Modifica categoria <?=  $cercato['nome']?></h2>
+    <h3>Modifica categoria <?=  $cercato['nome']?></h3>
     <hr>
     <form method="post" action="categoria_mod_r.php">
       <input type="hidden" name="id_mod" value="<?= $id_mod ?>"/>
       <label for="nome_c">Nome</label> <input value="<?= $cercato['nome'] ?>" placeholder="Inseri il nome della categoria" type="text" maxlength=50 id="nome_c" name="nome_c" required/>
-      <label for="descrizione_c">Descrizione</label> <textarea id="descrizione_c" name="descrizione_c"><?= $cercato['descrizione'] ?></textarea>
+      <label for="descrizione_c">Descrizione</label>
+        <textarea id="descrizione_c" name="descrizione_c"><?= $cercato['descrizione'] ?></textarea>
 
       <input type="submit" value="Conferma">
       <input type="reset" value="Annulla">
