e7fd01958b7b520e39064e4072933db3ac75d803
CristianMaschio <blackspartan@hotmail.it>
2017-11-27 13:05:48 +0100

    Inizio del css per mobile

---
+++ Index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+
+<?php require_once('php/php/config.php'); ?>
+<?php require_once('php/php/printTemplate.php') ?>
+
+<html lang="it" >
+<head>
+    <?= printHead('Home'); ?>
+</head>
+<body>
+
+<header>
+    <?= printHeader(); ?>
+</header>
+
+<nav>
+    <?= printNavBar(); ?>
+</nav>
+
+<?php echo consumeMessage(); ?>
+<div id="title"><h2><hr>Categorie di eventi disponibili!</h2></div>
+<hr>
+<div id="content">
+
+    <dl>
+        <?php $categorie = select("
+                SELECT *
+                FROM categorie
+                ORDER BY id DESC
+                ");
+            foreach($categorie as $c){
+                echo "<dt><a href=\"categoria_scheda.php?cat_id=".$c['id']."\">".$c['nome']."</a></dt> <dd>".$c['descrizione']."</dd>";
+        }
+        ?>
+    </dl>
+</div>
+
+<footer>
+    <?= printFooter(); ?>
+</footer>
+</body>
+</html>
