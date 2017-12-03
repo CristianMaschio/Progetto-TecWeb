20cd1854083d753d841ac582f0a5e91e738aba09
CristianMaschio <blackspartan@hotmail.it>
2017-12-03 11:09:16 +0100

    Add css to registrazione_utente_luogo.php & registrazione.php

--- php/registrazione.php
+++ php/registrazione.php
@@ -31,29 +31,32 @@ tipo_r
 
   <?php echo consumeMessage(); ?>
 
-  <div id="content" class="box">
-    <h2>Registrazione</h2>
+  <div id="content" class="contentBox">
+      <div class="box">
+          <h2>Registrazione</h2>
 
-    <form action="registrazione_r.php" method="POST" name="form">
-      <label lang="en" for="username">Username</label>
-        <input id="username" type="text" name="username_r" REQUIRED />
-      <label lang="en" for="password">Password</label>
-        <input id="password" type="password" name="password_r" REQUIRED />
+        <form action="registrazione_r.php" method="POST" name="form">
+          <label lang="en" for="username">Username</label>
+            <input id="username" type="text" name="username_r" REQUIRED />
+          <label lang="en" for="password">Password</label>
+            <input id="password" type="password" name="password_r" REQUIRED />
 
-      <label for="nome">Nome</label>
-        <input id="nome" type="text" name="nome_r"  placeholder="Inserisci il tuo nome" REQUIRED>
+          <label for="nome">Nome</label>
+            <input id="nome" type="text" name="nome_r"  placeholder="Inserisci il tuo nome" REQUIRED>
 
-      <label for="cognome">Cognome</label>
-        <input id="cognome" type="text" name="cognome_r" placeholder="Inserisci il tuo cognome" REQUIRED>
+          <label for="cognome">Cognome</label>
+            <input id="cognome" type="text" name="cognome_r" placeholder="Inserisci il tuo cognome" REQUIRED>
 
-      <label lang="en" for="email">Email</label>
-        <input id="email" type="email" name="email_r" placeholder="esempio@esempio.com" REQUIRED>
+          <label lang="en" for="email">Email</label>
+            <input id="email" type="email" name="email_r" placeholder="esempio@esempio.com" REQUIRED>
 
-      <input type="hidden" name="tipo_r" value="U"><hr>
-      <input type="submit" value="Completa registrazione">
-      <input type="reset" value="Azzera campi">
-    </form>
-
+          <input type="hidden" name="tipo_r" value="U"><hr>
+            <div class="boxInline">
+              <input type="submit" value="Completa registrazione">
+              <input id="buttonRight" type="reset" value="Azzera campi">
+            </div>
+        </form>
+      </div>
   </div>
 
   <footer>
--- php/registrazione_utente_luogo.php
+++ php/registrazione_utente_luogo.php
@@ -22,31 +22,36 @@
 
   <?php echo consumeMessage(); ?>
 
-  <div id="content">
-  <h2>Aggiungi nuov utente amministratore di luogo</h2><hr>
+  <div id="content" class="contentBox">
+      <div class="box">
 
-  <form action="registrazione_utente_luogo_r.php" method="POST" name="form">
-            <label for="username_r">Username</label> <input type="text" name="username_r" REQUIRED>
-            <label for="password_r">Password</label> <input type="password" name="password_r" REQUIRED>
-            <label for="nome_r">Nome</label> <input type="text" name="nome_r"  placeholder="Inserisci il nome" REQUIRED>
-            <label for="cognome_r">Cognome</label> <input type="text" name="cognome_r" placeholder="Inserisci il cognome" REQUIRED>
-            <label for="email_r">Email</label><input type="email" name="email_r" placeholder="esempio@esempio.com" REQUIRED>
-            <label for="luogo_r">Luogo</label><select name="luogo_r" required>
-                <?php $luoghi = select("SELECT * FROM luoghi ORDER BY nome ASC");
-                foreach($luoghi as $l){
-                    echo "<option value=".$l['id'].">".$l['nome']."</option>";
-                }
-                ?>
+          <h2>Aggiungi nuov utente amministratore di luogo</h2>
 
-                <?php
-                    $_SESSION['messaggio_registrazione'] = "Amministratore di luogo aggiunto correttamente";
-                    $_SESSION['redirect_registrazione'] = "utente_scheda.php?id_u=".$_SESSION['user_id'];
-                ?>
-            <input type="hidden" name="tipo_r" value="L"><hr>
-            <!-- NON PUOI MANDARE A REGISTRAZIONE_R STA ROBA, DEVI FARE UNA PAGINA R APPOSTA -->
-            <input type="submit" value="Completa registrazione">
-            <input type="reset" value="Azzera campi">
-        </form>
+          <form action="registrazione_utente_luogo_r.php" method="POST" name="form">
+                <label for="username_r">Username</label> <input type="text" name="username_r" REQUIRED>
+                <label for="password_r">Password</label> <input type="password" name="password_r" REQUIRED>
+                <label for="nome_r">Nome</label> <input type="text" name="nome_r"  placeholder="Inserisci il nome" REQUIRED>
+                <label for="cognome_r">Cognome</label> <input type="text" name="cognome_r" placeholder="Inserisci il cognome" REQUIRED>
+                <label for="email_r">Email</label><input type="email" name="email_r" placeholder="esempio@esempio.com" REQUIRED>
+                <label for="luogo_r">Luogo</label><select name="luogo_r" required>
+                    <?php $luoghi = select("SELECT * FROM luoghi ORDER BY nome ASC");
+                    foreach($luoghi as $l){
+                        echo "<option value=".$l['id'].">".$l['nome']."</option>";
+                    }
+                    ?>
+
+                    <?php
+                        $_SESSION['messaggio_registrazione'] = "Amministratore di luogo aggiunto correttamente";
+                        $_SESSION['redirect_registrazione'] = "utente_scheda.php?id_u=".$_SESSION['user_id'];
+                    ?>
+                <input type="hidden" name="tipo_r" value="L">
+                <!-- NON PUOI MANDARE A REGISTRAZIONE_R STA ROBA, DEVI FARE UNA PAGINA R APPOSTA -->
+              <div class="boxInline">
+                <input type="submit" value="Completa registrazione">
+                <input id="buttonRight" type="reset" value="Azzera campi">
+              </div>
+          </form>
+      </div>
 
   </div>
 
