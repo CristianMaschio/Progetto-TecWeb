50202653e8982490f37a1a26ca712e8b7fb5721a
Unknown <parolini.francesco.1@gmail.com>
2017-12-05 15:23:36 +0100

    Evidenziati in rosso errori nella pagina info.php

--- php/info.php
+++ php/info.php
@@ -21,6 +21,8 @@
 	<div id="content">
         <div class="title"><h2>Informazioni</h2></div>
             <div class="content">
+
+						<span style="color: red">Errori in tutta la pagina: l'attributo lingua negli span non è xml:lang (che è di XHTML), ma lang e basta. clicka in italiano si scrive clicca.</span>
 				<h4>Chi siamo</h4>
 				<div class="content">
 					Benvenuto nella Biglietteria <span xml:lang="eng">Online</span>. Qui potrai prenotare il tuo posto in un ampia fascia di <a href="categorie.php">categorie</a> di eventi, come biglietti per musei, fiere o cinema. 
@@ -41,49 +43,26 @@
 					Se hai già completato la procedura di registrazione e vuoi entrare nel nostro sistema non devi fare altro che clickare sul <span xml:lang="eng">link</span> "<span xml:lang="eng">Login</span>/Registrazione". Dopodichè dovrai inserire i tuoi dati ed entrerai nel sistema.
 				</div>
 
-				<h4>Come ricercare uno spettacolo</h4>
+				<h4>Come cercare uno spettacolo</h4>
+				<span style="color: red">chiarisci il concetto di spettacolo: istanziazione di evento</span>
 				<div class="content">
-					Attraverso la barra di ricerca posta in alto a destra puoi ricercare tutti gli Spettacoli di una certa categoria digitando il nome della categoria (ad esempio: "Cinema"), oppure gli Spettacoli che si svolgeranno in un certo luogo digitando il nome del luogo (ad esempio: "Porto Astra"). Puoi anche ricercare uno Spettacolo direttamente con il suo nome! Dopo aver effetuato la ricerca ti si presenterà davanti una tabella contenente i risultati. Nella tabella puoi clickare sui vari campi per ottenere ulteriori informazioni riguardo il determinato campo. Se ad esempio hai ricercato "Van Gogh" e vuoi ottenere ulteriori informazioni sulla <span xml:lang="eng">location</span> oppure altri eventi in musei ti bastera clickare rispettivamente sulle rispettive voci. Se vuoi procedere ad effettuare una prenotazione invece clicka sul nome dell'evento. Se infine sei interessato a vedere tutti gli Eventi, i Luoghi e le Categorie puoi clickare sul <span xml:lang="eng">link</span> rispettivo proprio sotto il nostro logo.
+					Attraverso la barra di ricerca posta in alto a destra puoi ricercare tutti gli Spettacoli di una certa categoria digitando il nome della categoria (ad esempio: "Cinema"), oppure gli Spettacoli che si svolgeranno in un certo luogo digitando il nome del luogo (ad esempio: "Porto Astra"). Puoi anche ricercare uno Spettacolo direttamente con il suo nome! <span style="color: red">gli spettacoli non hanno un nome, sono l'incrocio dei due assi eventi e luoghi</span> Dopo aver effetuato la ricerca ti si presenterà davanti una tabella contenente i risultati. Nella tabella puoi clickare sui vari campi per ottenere ulteriori informazioni riguardo il determinato campo. Se ad esempio hai ricercato "Van Gogh" e vuoi ottenere ulteriori informazioni sulla <span xml:lang="eng">location</span> oppure altri eventi in musei ti bastera clickare rispettivamente sulle rispettive voci. Se vuoi procedere ad effettuare una prenotazione invece clicka sul nome dell'evento. Se infine sei interessato a vedere tutti gli Eventi, i Luoghi e le Categorie puoi clickare sul <span xml:lang="eng">link</span> rispettivo proprio sotto il nostro logo nella barra dei menu.
 				</div>
 
-				<h4>Come prenotare</h4>
+				<h4>Come prenotare un biglietto</h4>
 				<div class="content">
 					Dopo aver effettuato una ricerca clicka sul nome dell'evento a cui vuoi partecipare per procedere alla prenotazione di un biglietto. Dopo aver clickato ti si presenterà davanti una tabella contenente tutte le date disponibili. Clicka nel piccolo pulsante con scritto "Prenota" per assicurarti un posto.
+				</div>
+
+				<h4>Come vedere ed annullare le tue prenotazioni di biglietti</h4>
+				<div class="content">
+					Clicka sul tuo <span xml:lang="eng">username</span> in alto a destra sotto la barra di ricerca: ti si aprirà la tua pagina personale. Qui potrai vedere le tue prenotazioni. Se ne vuoi cancellare qualcuna ti basterà clickare il pulsante "Annulla prenotazione" nella tabella.
 				</div>
 
 				<h4>Come vedere e modificare i tuoi dati</h4>
 				<div class="content">
 					Clicka sul tuo <span xml:lang="eng">username</span> in alto a destra sotto la barra di ricerca: ti si aprirà la tua pagina personale. Qui potrai vedere i tuoi dati personali. Se vuoi cambiare alcuni dati ti basterà clickare sul <span xml:lang="eng">link</span> "Modifica informazioni utente", da qui potrai cambiare i tuoi dati inserendoli in un piccolo <span xml:lang="eng">form</span> e poi confermando le modifiche.
 				</div>
-
-				<h4>Come vedere ed annullare le tue prenotazioni</h4>
-				<div class="content">
-					Clicka sul tuo <span xml:lang="eng">username</span> in alto a destra sotto la barra di ricerca: ti si aprirà la tua pagina personale. Qui potrai vedere le tue prenotazioni. Se ne vuoi cancellare qualcuna ti basterà clickare il pulsante "Annulla prenotazione" nella tabella.
-				</div>
-
-
-		<!--
-		TODO: cancellare questa vecchia info con pdf come guida
-		<div class="content">
-            <p>
-				Benvenuto nella biglietteria <span xml:lang="eng">online</span>. &Egrave; qui possibile prenotare biglietti riguardanti varie <a href="categorie.php">categorie</a>, come biglietti per musei, fiere o cinema.
-                Inoltre se vuoi prenotare un determinato evento potrai cercare se &egrave; disponibile nell' apposita <a href="eventi.php">pagina</a>.
-                Utilizzare il nostro servizio ti permette di assicurarti un posto a uno qualunque
-                degli spettacoli proposti senza dover pagare in anticipo o recarti a ritirare il biglietto in qualsiasi luogo.
-                Se ancora non hai un <span xml:lang="eng">account</span> <a href="registrazione.php">registrati</a> gratuitamente
-                per poter prenotare dei biglietti. Una volta eseguito il <span xml:lang="eng">login</span>(grazie al menu a sinistra) potrai prenotare biglietti per gli eventi.
-                Ogni biglietto &egrave; identificato da un codice univoco che lo identifica: stampa tale codice con relativo nome utente dell' <span xml:lang="eng">account</span>
-                e presentati al posto e all' ora dello spettacolo
-                con esso, gli addetti verificheranno le informazioni e potrai immediatamente entrare! Per ultriori informazioni
-                &egrave; possibile chiamare il numero +39 340 1234567, inviare un' email a
-                <a href="mailto:biglietteria@biglietteria.it">biglietteria@biglietteria.it</a>,
-                oppure venire alla nostra sede in Via Garibaldi n.2, Padova PD. Per una guida completa clicca
-                <a href="pdf/Manuale utente.pdf">qui</a>.
-            <hr><img src="img/biglietto.png">
-            <i>Troverai questa tabella nella pagina del profilo utente</i>
-            </p>
-        </div>
-		-->
             </div>
 	</div>
 
