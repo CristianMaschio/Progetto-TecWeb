099a4c9ab440ba0ea3a1697f548b3946e785e49e
Unknown <paolo.eccher96@gmail.com>
2017-12-26 16:03:31 +0100

    TABINDEx

--- php/registrazione_utente_luogo.php
+++ php/registrazione_utente_luogo.php
@@ -27,17 +27,17 @@
       <div class="box">
           <form action="registrazione_utente_luogo_r.php" method="POST" name="form">
                 <label for="username_r">Username</label>
-                <input type="text" id="username_r" name="username_r" REQUIRED>
+                <input tabindex=10 type="text" id="username_r" name="username_r" REQUIRED>
                 <label for="password_r">Password</label>
-                <input type="password" id="password_r" name="password_r" REQUIRED>
+                <input tabindex=20 type="password" id="password_r" name="password_r" REQUIRED>
                 <label for="nome_r">Nome</label>
-                <input type="text"  id="nome_r" name="nome_r"  placeholder="Inserisci il nome" REQUIRED>
+                <input tabindex=30 type="text"  id="nome_r" name="nome_r"  placeholder="Inserisci il nome" REQUIRED>
                 <label for="cognome_r">Cognome</label>
-                <input type="text"  id="cognome_r" name="cognome_r" placeholder="Inserisci il cognome" REQUIRED>
+                <input tabindex=40 type="text"  id="cognome_r" name="cognome_r" placeholder="Inserisci il cognome" REQUIRED>
                 <label for="email_r">Email</label>
-                <input type="email"  id="email_r"name="email_r" placeholder="esempio@esempio.com" REQUIRED>
+                <input tabindex=50 type="email"  id="email_r"name="email_r" placeholder="esempio@esempio.com" REQUIRED>
                 <label for="luogo_r">Luogo</label>
-                <select  id="luogo_r" name="luogo_r" required>
+                <select tabindex=60  id="luogo_r" name="luogo_r" required>
                     <?php $luoghi = select("SELECT * FROM luoghi ORDER BY nome ASC");
                     foreach($luoghi as $l){
                         echo "<option value=".$l['id'].">".$l['nome']."</option>";
@@ -49,7 +49,7 @@
                         $_SESSION['redirect_registrazione'] = "utente_scheda.php?id_u=".$_SESSION['user_id'];
                     ?>
                 <input type="hidden" name="tipo_r" value="L">
-                <!-- NON PUOI MANDARE A REGISTRAZIONE_R STA ROBA, DEVI FARE UNA PAGINA R APPOSTA -->
+                <!-- TODO: NON PUOI MANDARE A REGISTRAZIONE_R STA ROBA, DEVI FARE UNA PAGINA R APPOSTA -->
               <div class="boxInline">
                 <input type="submit" value="Completa registrazione">
                 <input id="buttonRight" type="reset" value="Azzera campi">
